b36d0eae623343abbc75504517e9e600b51dc1d2
Jason Gonzales <jason.gonzales23@gmail.com>
2014-03-29 15:48:32 -0700

    add geolocation stuffs

--- ch8/app/scripts/main.js
+++ ch8/app/scripts/main.js
@@ -16,6 +16,7 @@ var renderTemplate = function(target, itemId) {
 	//var data = {title: "This is the title", body: "This is the body!"};
 	// actual data!
 	var data;
+
 	if (itemId) {
 		var arr = $.grep( beers, function(beer, i){
 			return beer.id == itemId;
@@ -28,6 +29,9 @@ var renderTemplate = function(target, itemId) {
 	}
 	var html = template(data);
 	$(target).html(html);
+  if ($(target).find('#add-location').length > 0){
+    getCurrentLocation(renderCurrentLocation);
+  }
 };
 
 $.fn.serializeObject = function()
@@ -50,6 +54,9 @@ $.fn.serializeObject = function()
 var saveEntry = function(result) {
   var highestId = Math.max.apply(Math, beers.map(function(o){return o.id;}));
   result.id = highestId > 0 ? highestId + 1 : 1;
+  if (result.location){
+    result.location = JSON.parse(result.location);
+  }
   beers.push(result);
   localStorage.beers = JSON.stringify(beers);
 };
@@ -118,6 +125,25 @@ var isOnline = function () {
 var isOffline = function ()   {
   displayOnlineStatus.innerHTML = "Offline";
   displayOnlineStatus.className = "label-danger label pull-right";
+};
+
+var renderCurrentLocation = function(location) {
+  var target = $('#add-location');
+  var source = $('#add-location-template').html();
+  var template = Handlebars.compile(source);
+  location.string = JSON.stringify(location.coords);
+  var data = {location: location};
+  var html = template(data);
+  $(target).html(html);
+}
+
+var getCurrentLocation = function(cb) {
+  if(Modernizr.geolocation){
+    navigator.geolocation.getCurrentPosition(cb);
+  }
+  else {
+    //do something else
+  }
 };
 
 //event listeners
